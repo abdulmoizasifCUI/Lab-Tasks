175da68308ba83de83d9e2cf7b95bfedee6a705a
Abdul Moiz Asif <FA24-BCS-123@cuilahore.edu.pk>
2025-03-31 19:44:08 +0500

    Added these files

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Product prod1 = new Product("Rooh Afza", 1200.0);
+        Product prod2 = new Product("Shan Masala", 600.0);
+
+        Product.setDiscount(200);
+
+        prod1.calculateDiscountedPrice();
+        prod2.calculateDiscountedPrice();
+
+
+        System.out.println("Product: " + prod1.productName + " Original Price: " + prod1.price + " Discounted Price: " + prod1.calculateDiscountedPrice());
+        System.out.println("Product: " + prod2.productName + " Original Price: " + prod2.price + " Discounted Price: " + prod2.calculateDiscountedPrice());
+        System.out.println("Product " + prod1.compareDisocunt(prod2) + " has a higher discounted price.");
+    }
+}
